d6dee00c1a274a16bb01e670d339d6a9c9c85153
Gabriel Pato <gabriel@kemiocode.com>
2024-03-24 23:45:54 -0300

    novos foes, nova estrutura de folders, reciclando scripts de comportamento

--- main/foes/chopper.go
+++ main/foes/chopper.go
@@ -1,24 +1,4 @@
 components {
-  id: "circular_patrol"
-  component: "/main/behavior/circular_patrol.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-  properties {
-    id: "speed"
-    value: "25.0"
-    type: PROPERTY_TYPE_NUMBER
-  }
-}
-components {
   id: "explode"
   component: "/main/behavior/explode.script"
   position {
@@ -33,10 +13,40 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "circular_patrol"
+  component: "/main/behavior/circular_patrol.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "foe_id"
+    value: "4.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "speed"
+    value: "200.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "base_distance"
+    value: "150.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"tank-down\"\n"
+  data: "default_animation: \"chopper-down\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   "textures {\n"
@@ -70,8 +80,8 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 0.0\n"
-  "      y: 0.0\n"
+  "      x: -9.0\n"
+  "      y: 7.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -84,8 +94,8 @@ embedded_components {
   "    count: 3\n"
   "    id: \"\"\n"
   "  }\n"
-  "  data: 8.323354\n"
-  "  data: 11.077845\n"
+  "  data: 15.294341\n"
+  "  data: 15.0690365\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
