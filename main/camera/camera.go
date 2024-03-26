440cd0cda3c083e8758f4f9b0b8f1ef59ca21cb8
Guilherme Garcia <guilherme.pato.garcia@gmail.com>
2024-03-25 23:59:38 -0300

    camera filha do player

---
+++ main/camera/camera.go
@@ -0,0 +1,38 @@
+components {
+  id: "camera1"
+  component: "/main/camera/camera.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "camera"
+  type: "camera"
+  data: "aspect_ratio: 1.78\n"
+  "fov: 0.7854\n"
+  "near_z: -1.0\n"
+  "far_z: 100.0\n"
+  "auto_aspect_ratio: 1\n"
+  "orthographic_projection: 1\n"
+  "orthographic_zoom: 2.5\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
